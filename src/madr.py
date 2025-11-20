0916e21a6fcf66a1d6740cbcc010c419e3289ab9
edfournier <ericfournier.d@gmail.com>
2025-11-20 11:57:50 -0500

    rename madr prompt files

--- src/madr.py
+++ src/madr.py
@@ -2,17 +2,17 @@ from .model_clients import ModelClient
 from .parsers import parse_boolean
 
 def run_madr(mc: ModelClient, explanation: str, claim: str, qa_pairs: list[tuple[str, str]], max_iters: int = 3):
-    f1 = mc.send_prompt("init_feedback1", [explanation, claim, qa_pairs])
-    f2 = mc.send_prompt("init_feedback2", [explanation, claim, qa_pairs])
+    f1 = mc.send_prompt("madr_init_fb1", [explanation, claim, qa_pairs])
+    f2 = mc.send_prompt("madr_init_fb2", [explanation, claim, qa_pairs])
 
     for i in range(max_iters):
-        judgement =  mc.send_prompt("judge_feedback", [f1, f2])
+        judgement =  mc.send_prompt("madr_judge", [f1, f2])
         if parse_boolean(judgement):
             break
         
         if i != max_iters - 1:
-            f1 = mc.send_prompt("cross_feedback", [f1, f2])
-            f2 = mc.send_prompt("cross_feedback", [f2, f1])
+            f1 = mc.send_prompt("madr_cross_fb", [f1, f2])
+            f2 = mc.send_prompt("madr_cross_fb", [f2, f1])
 
-    revised_verdict = mc.send_prompt("revise_stop_check", [explanation, f1, f2])
+    revised_verdict = mc.send_prompt("madr_revise", [explanation, f1, f2])
     return revised_verdict
